a312b701e00353786a2092a0f3fecc16d6449b90
ThamsanqaMpontshane <thamsanqalm01@gmail.com>
2022-08-20 01:45:08 +0200

    minor changes

--- mydatabase.sql
+++ mydatabase.sql
@@ -2,7 +2,7 @@ Create table towns (
     id serial PRIMARY KEY,
     name varchar(255) NOT NULL,
     plate varchar(255) NOT NULL
-)
+);
 Create table reg_numbers (
     id serial primary key,
     reg_number varchar(255) not null,
