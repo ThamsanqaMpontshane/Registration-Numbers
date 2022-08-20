30b621ca0ca2edd212faedc43308c3f63af359bc
ThamsanqaMpontshane <thamsanqalm01@gmail.com>
2022-08-20 15:03:13 +0200

    minor changes

--- mydatabase.sql
+++ mydatabase.sql
@@ -3,6 +3,12 @@ Create table towns (
     name varchar(255) NOT NULL,
     plate varchar(255) NOT NULL
 );
+
+Insert into towns (name, plate) values ('Cape Town', 'CA');
+Insert into towns (name, plate) values ('Bellville', 'CY');
+Insert into towns (name, plate) values ('Paarl', 'CL');
+
+
 Create table reg_numbers (
     id serial primary key,
     reg_number varchar(255) not null,
